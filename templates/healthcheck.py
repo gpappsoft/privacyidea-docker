6f60ec379c8ba09ca595c5dd17f6d23c4b6cf0dd
Moeni <88508822+gpappsoft@users.noreply.github.com>
2025-02-28 11:51:16 +0100

    - fixed healthcheck.py

--- templates/healthcheck.py
+++ templates/healthcheck.py
@@ -3,7 +3,7 @@ import requests
 import os
 
                          
-BASE_URL= "http://localhost" + os.getenv('HOSTNAME') + ":" + os.getenv('PI_PORT') + "/healthz/" 
+BASE_URL= "http://" + os.getenv('HOSTNAME') + ":" + os.getenv('PI_PORT') + "/healthz/" 
 
 response = requests.get(BASE_URL)
 assert response.status_code == 200, "Unexpected status code: " + str(response.status_code)
